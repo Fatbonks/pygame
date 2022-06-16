88545f424cdf6601caa4ed6c676912ec39f1e58f
Fatbonks <antrish13@gmail.com>
2022-06-17 03:51:33 +1200

    redoing all of it :(

--- pythonGame.py
+++ pythonGame.py
@@ -1,45 +1,82 @@
 import random
-player_health = 10
-player_damage = 1
-is_event_happen = 0
-enemy_health = 10
-enemy_damage = 1
-
-print("welcome to my game")
 
 
-def create_event():
-    random.seed()
-    choice = random.randint(1, 2)
-    if choice == 2:
-        print("An enemy!")
-        enemy()
-    if choice == 1:
-        print("chest!")
-
-def enemy():
-    while enemy_health > 0 or player_health > 0:
-        print(enemy_health = 10)
-        print( enemy_damage = 1)
-        attack_first = random.randint(0, 1)
+# import time
+# import sys
+# import os
 
 
+# map #
+class ZoneMap:
+    def __int__(self):
+        self.left = False
+        self.right = False
+        self.down = True
 
-help = "how to play: up,down,left,right to move your character.\ninput 'player' to see your stats\nand 'help' will give you list of commands you can text"
-print(help)
 
-game_is_running = True
+zone_map = ZoneMap()
 
-while game_is_running:
-    player = input("Input here: ").lower().strip()
-    if player == "player":
-        print("{} health, {} damage".format(player_health, player_damage))
-    if player == "up":
-        random.seed()
-        is_event_happen = random.randint(0, 1)
-        if is_event_happen == 1:
-            create_event()
-    if player == "help":
-        print(help)
 
-  
+# enemy_class #
+class Enemy:
+    def __int__(self):
+        self.health = 0
+        self.damage = 0
+        self.name = ''
+
+
+enemy = Enemy()
+
+
+# player_class #
+class Player:
+    def __int__(self):
+        self.name = ''
+        self.hp = 10
+        self.damage = 1
+        self.effects = []
+
+
+my_player = Player()
+
+
+def spawn_enemy():
+    enemy_names = ['goblin', 'troll', 'wolf']
+    enemy.name = random.choice(enemy_names)
+    if enemy.name == 'goblin':
+        enemy.health = 10
+        enemy.damage = 1
+    if enemy.name == 'wolf':
+        enemy.health = 5
+        enemy.damage = 1
+    if enemy.name == 'troll':
+        enemy.health = 15
+        enemy.damage = 2
+    print(enemy.name)
+
+
+def start_game():
+    wall = False
+    game_is_running = True
+    while game_is_running:
+        move = input("> ").lower()
+        if move == 'up':
+            print(move)
+        if move == 'left' and wall == True:
+            print(move)
+        elif move == 'left' and wall == False:
+            print("There is a wall blocking your path")
+
+        if move == 'right':
+            print(move)
+        if move == 'down':
+            print(move)
+
+
+# print("What is your name?")
+
+# my_player.name = input('> ')
+
+# print("Ah, i see {}. What a nice name!".format(my_player.name))
+
+start_game()
