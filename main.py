69e03f9da2a41484773f1ec10660e871c8651f05
nodlefam32 <eldon9163@gmail.com>
2022-06-22 14:21:41 +1200

    added player dictionary

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ class game_logic:
     def __init__(self):
         self.name = ''
         self.number = 0
+        self.player = {"class": {"warrior": {"stats": ({"health": 10.0}, {"mana": 10.0})}}}
+
 
     def add_one(self):
         self.number += 1
-        print(self.number)
+        print(self.player.get("class").get("warrior"))
 
 if __name__ == '__main__':
     game = game_logic()
-    game.add_one()  
+
+    
+
+    # Main game loop
+    while True:
+        game.add_one()  
 
