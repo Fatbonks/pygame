9c9ce0759c23a2e40c325143576bfb933a55eb3f
Fatbonks <antrish13@gmail.com>
2022-06-23 01:15:04 +1200

    i have done it! added all the players stats in!

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-class game_logic:
+class GameLogic:
     # Declare all global variables 
     def __init__(self):
         self.name = ''
-        self.number = 0
-        self.player = {"class": {"warrior": {"stats": ({"health": 10.0}, {"mana": 10.0})}}}
-
+        self.player = {'class': {'warrior': {'stats': {'health': 13.0, 'mana': 5.0,
+                                                       'damage': {'min_damage': 1.0, 'max_damage': 5.0},
+                                                       'speed': 1, 'dodge': 0},
+                                             'level': {'level': 1, 'level_next': 25, 'exp': 0}},
+                                 'mage': {'stats': {'health': 5.0, 'mana': 5.0,
+                                                    'damage': {'min_damage': 1, 'max_damage': 3},
+                                                    'speed': 2, 'dodge': 0},
+                                          'level': {'level': 1, 'level_next': 25, 'exp': 0}},
+                                 'archer': {'stats': {'health': 13.0, 'mana': 5.0,
+                                                      'damage': {'min_damage': 1.0, 'max_damage': 5.0},
+                                                      'speed': 4, 'dodge': 0},
+                                            'level': {'level': 1, 'level_next': 25, 'exp': 0}},
+                                 'thief': {'stats': {'health': 13.0, 'mana': 5.0,
+                                                     'damage': {'min_damage': 1.0, 'max_damage': 5.0},
+                                                     'speed': 3, 'dodge': 25},
+                                           'level': {'level': 1, 'level_next': 25, 'exp': 0}}}}
 
     def add_one(self):
-        self.number += 1
-        print(self.player.get("class").get("warrior"))
+        print(self.player['class']['warrior'])
+
 
 if __name__ == '__main__':
-    game = game_logic()
-
-    
-
+    game = GameLogic()
+    game.add_one()
     # Main game loop
     while True:
-        game.add_one()  
+        pass
+
 
