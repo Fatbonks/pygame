ae283a120f4740490272bb75995ea8fb53c64a35
Fatbonks <antrish13@gmail.com>
2022-07-02 01:10:51 +1200

    run main.py to play!

--- GameData.py
+++ GameData.py
@@ -37,11 +37,27 @@ class GameData:
         }
 
         # Dictionary for the enemies of the player
-        self.enemies = {
+        self.blank_enemy = {
             'enemy': {
                 'name': '',
                 'stats': {'health': 0.0, 'damage': {'min_damage': 0.0, 'max_damage': 0.0}, 'speed': 0},
                 'level': {'level': 0, 'exp': 0}, 'drops': {'gold': 0}
+            }
+        }
+
+        self.enemy_names = [
+            'jeff, the angry miller', 'joe, the angry butcher', 'bobenstein', 'hobgoblin', 'lost Leo', 'Cody, the elf',
+            'dog', 'slime, the god destroyer', 'old man', 'smith, the smithing smither', 'smeckle man',
+            'bob, the horse', 'Milf destroyer 3000'
+        ]
+
+        self.enemy_stat_ranges = {
+            'enemy': {
+                'stats': {
+                    'low_health': 5, 'max_health': 13, 'number_1_min_damage': 1, 'number_1_max_damage': 2,
+                    'number_2_min_damage': 3, 'number_2_max_damage': 5, 'min_speed': 1, 'max_speed': 4, 'min_exp': 3,
+                    'max_exp': 13, 'min_gold': 2, 'max_gold': 14
+                          }
             }
         }
 
@@ -79,6 +95,7 @@ class GameData:
         self.names = ['Bill', 'John', 'Dave', 'Cow', 'Tiffany', 'Tod', 'Elliot', 'Mexican jesus', 'cody']
 
         self.has_healed = False
+
 
 
 
